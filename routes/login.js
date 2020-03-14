7baa1ed5f0046dd14ccc57a0461fd8d08e2e8b13
simba <simba.fs@gmail.com>
2020-03-14 13:04:21 +0800

    db

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,37 @@
-const router = require('express').Router();
+//(async () => {
+	const router = require('express').Router();
+	const passport = require('passport');
+	const LocalStrategy = require('passport-local').Strategy;
+	const sqlite = require('sqlite-async');
+	
+	const User = sqlite.open('../db/user.db')
+		.then((db) => {
+			Url.run(`CREATE if not exists TABLE User (
+				username STRING,
+				password STRING
+			)`);
+		})
+		.catch(console.error);
+	
+	router.get('/', (req, res, next) => {
+		console.log('/login');
+		res.render('login', {
+			status: 'login'
+		});
+	});
+	
+	/*
+	passport.use(new LocalStrategy((username, password) => {
+		
+	}));
+	*/
 
-router.get('/', (req, res, next) => {
-	res.render('login');
-});
-
-module.exports = router;
+	router.get('/auth', (req, res, next) => {
+		console.log('/login/auth');
+		res.render('login', {
+			status: 'fail'
+		});
+	});
+	
+	module.exports = router;
+//})();
