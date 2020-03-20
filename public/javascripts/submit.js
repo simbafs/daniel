664c33ee3a62d4fbf8aef387bd626cbb1c8a0f0e
simba <simba.fs@gmail.com>
2020-03-20 23:48:54 +0800

    submit

---
+++ public/javascripts/submit.js
@@ -0,0 +1,2 @@
+console.log('submit.js loaded');
+$('textarea').eq(1).autoResize();
--- routes/submit.js
+++ routes/submit.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
-	res.render('submit');
+	res.render('submit', {
+		now: 'kjsdadslkasd'
+	});
+});
+
+router.post('/', (req, res, next) => {
+	res.send(req.body);
 });
 
 module.exports = router;
