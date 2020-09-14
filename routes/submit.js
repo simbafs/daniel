725c9c511f79c8600efa728933f3050db9a1a9c6
simba-fs <simba.fs@gmail.com>
2020-09-14 23:24:34 +0800

    store comment

--- routes/submit.js
+++ routes/submit.js
@@ -2,27 +2,35 @@ const router = require('express').Router();
 const { load } = require('../setup/sqlite.js');
 const { auth } = require('../setup/auth.js');
 
-router.get('/', auth, (req, res, next) => {
+router.get('/', /*auth,*/ (req, res, next) => {
 	res.render('submit');
 });
 
 router.post('/', (req, res, next) => {
-	if(!req.isAuthenticated()) return res.send({
-		error: 'You have to login'
-	});
-	let author = req.user.username;
+	// if(!req.isAuthenticated()) return res.send({
+	//     error: 'You have to login'
+	// });
+	// let author = req.user.username;
+	let author = 'simba.fs@gmail.com';
 	let data = req.body.content
 		.split('\n')
 		.filter(item => item.length > 0)
-		.map(item => {return {
-			content: item,
-			author: author
-		}});
+		.map(item => {
+			item = item.split('//').map(i => i.trim(i));
+			return {
+				content: item[0],
+				comment: item[1] || '',
+				author: author
+			};
+		})
 	return load(data)
 		.then(() => res.send(data))
-		.catch(() => res.send({
-			error: 'Server error'
-		}));
+		.catch(e => {
+			console.error(e);
+			res.send({
+				error: 'Server error'
+			})
+		});
 });
 
 module.exports = router;
